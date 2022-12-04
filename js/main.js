a50ef7afaf516c7891c9ab87f3978fe7dbf8e006
Milos <milosstanisavljevic0107@gmail.com>
2022-12-04 23:25:23 +0100

    fixed hmg menu

--- js/main.js
+++ js/main.js
@@ -1,44 +1,47 @@
 ///Get data from mockapi
 
-function  fetchData(){
+function fetchData() {
     fetch("https://6321bddefd698dfa29fe03be.mockapi.io/products")
-    .then(response => {
-        console.log(response)
-        if (!response.ok){
-            throw Error('ERROR')
-        }
-        return response.json();
-    })
-    .then(data =>{
+        .then(response => {
+            console.log(response)
+            if (!response.ok) {
+                throw Error('ERROR')
+            }
+            return response.json();
+        })
+        .then(data => {
             let prices = document.querySelectorAll('.price');
-            prices.forEach(function(element,j) {
-              
+            prices.forEach(function (element, j) {
+
                 element.innerHTML += `<span id="price">${data[j].price}<span>$`
-            
-        
-            })    
+
+
+            })
         })
 
-    .catch(error =>{
-        console.log(error)
-    });
+        .catch(error => {
+            console.log(error)
+        });
 }
 
 fetchData();
 
 
 
-function hmgmenu(){
+function hmgmenu() {
     let menu = document.querySelector('#menu-icon');
     let navbar = document.querySelector('.navbar');
-    
+
     menu.onclick = () => {
         menu.classList.toggle('bx-x');
         navbar.classList.toggle('active');
     };
-    
+
     window.onscroll = () => {
         menu.classList.remove('bx-x');
         navbar.classList.remove('active');
     };
 }
+
+
+hmgmenu();
