821a17aca6dbc0289b75e6b95dc46efc08f055f3
Milos <milosstanisavljevic0107@gmail.com>
2022-12-21 21:31:59 +0100

    refactoring code

--- src/index.js
+++ src/index.js
@@ -1,24 +1,18 @@
 const express = require("express")
 const app = express()
 const path = require("path")
-const ejs = require("ejs")
 const mongodb = require("./mongodb")
 
-const objectid = mongodb.ObjectId
 const tempelatePath = path.join(__dirname, '../tempelates')
 
-// app.use(express.static("public"));
 app.use(express.json())
 app.set("view engine", "ejs")
-// app.set("view engine", "html")
 app.set("views", tempelatePath)
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', async (req, res) => {
 
     const products = await mongodb.productscollection.find();
-
-    console.log(products, 'MUSAAAAAA');
 
     res.render("home", {
         products: products
@@ -32,8 +26,6 @@ app.get('/sign', async (req, res) => {
 
 app.post("/sign", async (req, res) => {
 
-
-
     const data = {
         name: req.body.name,
         password: req.body.password
@@ -46,21 +38,16 @@ app.post("/sign", async (req, res) => {
 })
 app.get("/product", async (req, res) => {
 
-
-
     res.render("insertproducts")
 
 })
 
 app.post("/product", async (req, res) => {
 
-
-
     const data = {
         title: req.body.title,
         imageURL: req.body.imageURL,
         price: req.body.price
-
     }
 
     await mongodb.productscollection.insertMany([data])
@@ -72,18 +59,11 @@ app.post("/order", async (req, res) => {
 
     const { productid, quantity } = req.body
 
-    console.log(req.body);
-
     const { price } = await mongodb.productscollection.findById(productid)
-
-    console.log(price);
 
     await mongodb.orders.insertMany([{ productid, quantity, price: price * quantity }]);
 
     res.redirect("/orders")
-
-
-
 
 })
 app.get('/orders', async (req, res) => {
@@ -93,27 +73,21 @@ app.get('/orders', async (req, res) => {
     let totalprice = 0;
 
     let orders1 = orders.map((order) => {
-        totalprice += order.price;
 
+        totalprice += order.price;
 
         return {
             ...order, product: products.find((prod) => {
-                console.log(order.productid.toString());
-                console.log(prod._id.toString());
                 return order.productid.toString() === prod._id.toString();
-            }) || "zika"
+            }) || ""
         };
     });
-    console.log(orders1)
-
-
 
     res.render("orders", {
         orders: orders1,
         totalprice
     })
 })
-
 
 app.listen(3000, () => {
     console.log("PORT CONNECTED!!");
